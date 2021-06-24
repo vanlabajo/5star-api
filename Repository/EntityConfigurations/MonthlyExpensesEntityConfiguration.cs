125a3b6aa47fbda576dc39d97f36f3039cc6752f
Van Labajo <van.labajo@serialmultivision.com>
2021-06-24 15:22:30 +0800

    Updated MonthlySalesEntityConfiguration and MonthlyExpensesEntityConfiguration

--- Repository/EntityConfigurations/MonthlyExpensesEntityConfiguration.cs
+++ Repository/EntityConfigurations/MonthlyExpensesEntityConfiguration.cs
@@ -11,6 +11,19 @@ namespace Repository.EntityConfigurations
             builder.ToTable("MonthlyExpenses");
             builder.HasKey(o => o.Year);
             builder.Property(o => o.Year).ValueGeneratedNever();
+
+            builder.Property(o => o.Jan).HasPrecision(18, 6);
+            builder.Property(o => o.Feb).HasPrecision(18, 6);
+            builder.Property(o => o.Mar).HasPrecision(18, 6);
+            builder.Property(o => o.Apr).HasPrecision(18, 6);
+            builder.Property(o => o.May).HasPrecision(18, 6);
+            builder.Property(o => o.Jun).HasPrecision(18, 6);
+            builder.Property(o => o.Jul).HasPrecision(18, 6);
+            builder.Property(o => o.Aug).HasPrecision(18, 6);
+            builder.Property(o => o.Sep).HasPrecision(18, 6);
+            builder.Property(o => o.Oct).HasPrecision(18, 6);
+            builder.Property(o => o.Nov).HasPrecision(18, 6);
+            builder.Property(o => o.Dec).HasPrecision(18, 6);
         }
     }
 }
